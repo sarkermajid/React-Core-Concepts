c63c709b9c55dfae6f5ed14d73df02bd9c96b636
sarkermajid <majidursarkar333@gmail.com>
2022-05-31 16:53:25 +0600

    pass object data within an array and access

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 
 
 function App() {
+
+  const products = [
+    {name : 'Photoshop', price : '$90.99'},
+    {name : 'MS Word', price : '$20.99'},
+    {name : 'Vs Code', price : '$99.99'}
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,19 +17,24 @@ function App() {
         <Person name='Sarker Majid' profession='Software Engineer' address='77 Maniknagar, Dhaka-1203' education='CSE' gender='Male'></Person>
         <Person name='Sohan Khan' profession='Web Designer' address='Basabo' education='CSE' gender='Male'></Person>
         <Person name='Rumjhum' profession='Veppi' address='Mirpur' education='CSE' gender='Female'></Person>
+        <Product name={products[0].name} price={products[0].price}></Product>
+        <Product name={products[1].name} price={products[1].price}></Product>
+        <Product name={products[2].name} price={products[2].price}></Product>
       </header>
     </div>
   );
 }
-var profession = 'Laravel Developer';
-var education = 'B. Sc in Computer Science and Enginnering';
-var skills = {
+
+function Sarkermajid(){
+  var profession = 'Laravel Developer';
+  var education = 'B. Sc in Computer Science and Enginnering';
+  var skills = {
   'HTML' : 'HTML : Hyper Text Markup Language',
   'CSS' : 'CSS : Cascading Style Sheet',
   'JavaScript' : 'JavaScript : Modern Programming Language',
   'React_Js' : 'React Js : Library of JavaScript language',
-}
-function Sarkermajid(){
+  };
+  
   const sarkerStyle = {
     border : '5px solid #f1c40f',
     borderRadius : '30px',
@@ -56,6 +68,28 @@ function Person(props){
       <p>Address : {props.address}</p>
       <p>Education : {props.education}</p>
       <p>Gender : {props.gender}</p>
+    </div>
+  )
+}
+
+function Product(props){
+
+  const productStyle = {
+    border : '2px solid gray',
+    borderRadius : '5px',
+    backgroundColor : 'black',
+    color : 'yellow',
+    margin : '10px',
+    padding : '20px',
+    height : '200px',
+    width : '200px',
+    float : 'left'
+  }
+  return(
+    <div style={productStyle}>
+      <h2>{props.name}</h2>
+      <h5>{props.price}</h5>
+      <button>Buy now</button>
     </div>
   )
 }
