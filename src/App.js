0e691bba7673cad8b3e7723ff26f15eb74e0cbd6
sarkermajid <majidursarkar333@gmail.com>
2022-10-06 22:55:04 +0600

    create component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    username: 'sarker majid',
+    job: 'React js Developer'
+  };
+  var style = {
+    color: 'yellow',
+    backgroundColor: 'black',
+    padding: '20px'
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={style}>Md. Majidur Rahman Sarker <br />
+            Username : {person.username} <br />
+            Job : {person.job}
+        </h1>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+
+function Person(){
+  const personStyle = {
+    border: '2px solid red',
+    backgroundColor: 'black',
+    color: 'yellow',
+    margin: '10px',
+    padding: '20px'
+  } 
+  return (
+    <div style={personStyle}>
+      <h3>Sarker majid</h3>
+      <h5>Laravel Developer</h5>
+    </div>
+  )
 }
 
 export default App;
